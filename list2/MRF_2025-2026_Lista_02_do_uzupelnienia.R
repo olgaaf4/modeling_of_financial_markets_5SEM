cea2d386895a01b87fbda9bdf3c36312b9da90f8
Tomasz Warzecha <282261@student.pwr.edu.pl>
2025-10-13 18:40:51 +0200

    dodanie listy 2

---
+++ list2/MRF_2025-2026_Lista_02_do_uzupelnienia.R
@@ -0,0 +1,179 @@
+
+#-----------------*************** Lista 2 MRF ***************-------------------
+
+#_______________________________________________________________________________
+#
+#------------------------------Zadanie 2.1--------------------------------------
+#
+#_______________________________________________________________________________
+# Dłużnik spłaca pierwsza ratę długu 
+# na koniec pierwszego roku w wysokości 8 000 PLN oraz 
+# drugą na koniec drugiego roku w wysokości 6 000$ PLN. 
+# Jaka jest obecna wartość długu przy założeniu oprocentowania R = 4% ?
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+#_______________________________________________________________________________
+#
+#------------------------------Zadanie 2.2--------------------------------------
+#
+#_______________________________________________________________________________
+# Wpłacamy na konto dzisiaj 5000 PLN oraz przez kolejne 11 miesiecy po 
+# 1 000 PLN. Jaką kwotę na koncie ujrzymy na koniec roku, jeśli oprocentowanie 
+# w skali roku to R = 11% ? Zakładamy kapitalizację miesięczną i wpłaty 
+# na początku każdego kolejnego miesiąca.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+
+#_______________________________________________________________________________
+#
+#------------------------------Zadanie 2.3--------------------------------------
+#
+#_______________________________________________________________________________
+# Firma zainwestowała 5.0 mln PLN PLN w pewien projekt inwestycyjny, który 
+# przynosi następujące przepływy pieniezne: 1.0 mln PLN na koniec trzeciego 
+# roku, 2.0 mln PLN na koniec siódmego roku, 3.0 mln PLN na koniec 
+# dwunastego roku. Wyznacz IRR tej inwestycji.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+
+
+#_______________________________________________________________________________
+#
+#------------------------------Zadanie 2.4--------------------------------------
+#
+#_______________________________________________________________________________
+# Efektywna roczna stopa procentowa wynosi $15\%$. Podaj nominalna równoważną 
+# stope procentową przy kapitalizacji 
+#
+# a) kwartalnej,
+# b) miesięcznej,
+# c) dziennej,
+# d) ciągłej.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# podpunkt (a)
+
+
+# podpunkt (b)
+
+
+# podpunkt (c)
+
+
+# podpunkt (d)
+
+
+
+#_______________________________________________________________________________
+#
+#------------------------------Zadanie 2.5--------------------------------------
+#
+#_______________________________________________________________________________
+# Na rachunek (z kapitalizacja miesięczną) chcemy wpłacic 7000 PLN teraz oraz
+# co miesiac przez pół roku kwoty w wysokości 800 PLN. Jakie najniższe 
+# oprocentowanie w skali roku pozwoli nam zgromadzić po pół roku kwotę 
+# w wysokości 12000 PLN?
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+#_______________________________________________________________________________
+#
+#------------------------------Zadanie 2.6--------------------------------------
+#
+#_______________________________________________________________________________
+# Każda z osób A,B,C,D zaciąga kredyt w wysokości K na n okresów. 
+# Spłaty rat kredytu następują na koniec każdego z okresów. 
+# Oprocentowanie kredytu w skali okresu wynosi r, przy czym
+#
+# - A spłaca kredyt w jednakowych ratach,
+#
+# - B spłaca kredyt metodą "jednakowych rat kapitałowych"
+#   tzn. każda rata spłaty jest równa K/n plus odsetki od kwoty pozostałej 
+#   do spłaty na początku okresu,
+#
+# - C w pierwszych n-1 ratach spłaca jedynie odsetki za kończący się okres,
+#   a w ostatniej spłaca cały kapitał K plus odsetki za n-ty okres,
+#
+# - D spłaca całość zobowiązań na koniec n-tego okresu, przy czym 
+#   odsetki są kapitalizowane na koniec każdego okresu.
+#
+#   Napisz funkcję KalkulatorRatalny(TypeFlag = c("A", "B", "C", "D"), K, n, r)}
+#   zwracającą wektor wszystkich rat spłaty kredytu dla każdej z osób A,B,C,D.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+
+#_______________________________________________________________________________
+#
+#------------------------------Zadanie 2.7--------------------------------------
+#
+#_______________________________________________________________________________
+# Bank może nam udzielić kredytu w wysokości 16 000 PLN na okres 4 lat.
+# Możemy wybrac pomiędzy jednym z dwóch sposobów spłaty kredytu:
+#
+# - spłata kredytu w dwóch ratach po 10000 PLN każda 
+#   (na koniec drugiego i czwartego roku) 
+#
+# lub
+#
+# - spłata kredytu w czterech ratach po 5000 PLN każda.
+#
+# Ile wyniesie RRSO w obydwu propozycjach i która z propozycji spłaty 
+# jest dla nas korzystniejsza?
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+
+
+#_______________________________________________________________________________
+#
+#------------------------------Zadanie 2.8--------------------------------------
+#
+#_______________________________________________________________________________
+# Bank może nam udzielić kredytu w wysokości 200 000 PLN na okres 5 lat.
+# Kredyt będzie spłacany metodą równych rat kapitałowych tj. 
+# każda w wysokości 40 000 PLN, płatne na początku roku.
+#
+# Stopa procentowa wynosi 4%.
+#
+# W chwili t=0 pobierane są następujące opłaty
+#
+# - prowizja 6000 PLN,
+# - opłata przygotowawcza 2000 PLN,
+# - wycena zabezpiecenia 1000 PLN.
+# 
+# Ponadto na początku każdego okresu płacone jest również 
+# ubezpieczenie spłaty w wysokości 2% kwoty kredytu pozostałej do spłaty.
+#
+# Ile wynosi RRSO tego kredytu?
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+
+
